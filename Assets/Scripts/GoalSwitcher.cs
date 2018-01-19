7d265c41c2292d983491eecfd9b5d0d6b06f20b3
Bharat Gudihal <bharatgudihal@users.noreply.github.com>
2018-01-18 21:07:59 -0700

    init commit

---
+++ Assets/Scripts/GoalSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalSwitcher : MonoBehaviour {
+
+    public float goalSwitchTime = 2f;
+    private Goal goal_script;
+    private int goal_index = 0;
+    public List<GameObject> ordered_goals;
+    private float time_since_goal_switch = 0f;
+	// Use this for initialization
+
+    void Start()
+    {
+        goal_script = GetComponent<Goal>();
+        goal_script.setGoal(ordered_goals[goal_index]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time_since_goal_switch += Time.deltaTime;
+
+        if (time_since_goal_switch > goalSwitchTime)
+        {
+            goal_index = Random.Range(0, ordered_goals.Count);
+            goal_script.setGoal(ordered_goals[goal_index]);
+            time_since_goal_switch = 0f;
+        }
+	}
+}
